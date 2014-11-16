f48cd769077ba847cf5e4e9026d55607d4b6b54d
barrios <marco.heins@gmail.com>
2014-11-16 21:22:32 +0100

    bottle username ^ password

--- psom-fb.py
+++ psom-fb.py
@@ -1,10 +1,44 @@
-from bottle import route, run
+from bottle import get, post, request, run
 
-@route('/hello')
-def hello():
-    return 'Hello World'
 
-run(host='localhost', port=8080, debug=True)
+@get('/login')
+def login():
+    return '''
+        <form action="/login" method="post">
+        Username: <input name="username" type="text" />
+        Password: <input name="password" type="password" />
+        <input value="Login" type="submit" />
+        </form>
+    '''
+
+
+@post('/login')
+def do_login():
+    username = request.forms.get('username')
+    password = request.forms.get('password')
+    if check_login(username, password):
+        return "<p>Your login was correct, .</p>"
+    else:
+        return "<p>Login failed.</p>"
+
+
+def check_login(username, password):
+    if username == "Marco" and password == '1234':
+        return True
+
+
+run(host='localhost', port=8080)
+
+
+
+
+
+
+
+4$$%^^&*(())))))))))))_
+
+
+
 
 
 
